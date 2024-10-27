008050720842e1f853ec4f0562344dff3e7e2501
AJ <983097239@qq.com>
2024-10-27 18:09:23 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -14,9 +14,9 @@ git clone https://github.com/sbwml/luci-app-alist package/alist
 # => 阿里ddns
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
 
-# => passwall2
+# => passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 
 #主题
 rm -rf feeds/luci/themes/luci-theme-bootstrap
